730d1a596608406919aacb3747d915407e40b4e9
zhaotf <zhaotf@192.168.59.1>
2017-02-22 21:50:40 +0800

    springMVC+kafka 整合

---
+++ spring-kafka-producer1/src/main/java/test/KafkaProducerTest.java
@@ -0,0 +1,32 @@
+package test;
+
+import com.git.kafka.producer.KafkaProducerServer;
+
+/**
+ * 
+ * @ClassName: KafkaProducerTest
+ * @Description:
+ * @author: zhaotf
+ * @date: 2017年2月22日 下午9:47:07
+ */
+public class KafkaProducerTest {
+
+	public static void main(String[] args) {
+
+		KafkaProducerServer kafkaProducer = new KafkaProducerServer();
+		String topic = "orderTopic";
+		String value = "test";
+		String ifPartition = "0";
+		Integer partitionNum = 3;
+		String role = "test";// 用来生成key
+		Map<String, Object> res = kafkaProducer.sndMesForTemplate(topic, value,
+				ifPartition, partitionNum, role);
+
+		System.out.println("测试结果如下：===============");
+		String message = (String) res.get("message");
+		String code = (String) res.get("code");
+
+		System.out.println("code:" + code);
+		System.out.println("message:" + message);
+	}
+}
